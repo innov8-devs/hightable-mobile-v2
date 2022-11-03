6531f3f158ef981d8f08a7873693affe83b4e56d
AdioleDivine <adiledivine27@gmail.com>
2022-11-03 13:01:30 -0400

    Screen: Spalsscreen & Splashcreen animations

--- lib/modules/onboarding/views/screens/splash_screen.dart
+++ lib/modules/onboarding/views/screens/splash_screen.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hightable_mobile_v2/animations/fade_in_animation.dart';
 import 'package:hightable_mobile_v2/animations/slide_in_right.dart';
 import 'package:hightable_mobile_v2/animations/slide_left_animation.dart';
 import 'package:hightable_mobile_v2/utils/assets.dart';
@@ -53,15 +54,12 @@ class SplashScreenState extends ConsumerState<SplashScreen> {
                   bottom: 0,
                   child: SlideInRightAnimation(
                       delay: 0.5, child: Helpers.callSvg(SvgAssets.ellipse2))),
-              Positioned(
-                  left: 0,
-                  bottom: 60,
-                  child: SlideInLeftAnimation(
-                    delay: 0.5,
-                    child: Helpers.callPng(
-                      ImageAssets.logo,
-                    ),
-                  )),
+              Center(
+                child: FadeInAnimations(
+                  delay: 0.6,
+                  child: Helpers.callPng(ImageAssets.logo, height: 130),
+                ),
+              ),
               Positioned(
                   left: 0,
                   bottom: 60,
