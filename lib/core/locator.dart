f73e68810e48917bf04400b413df6c8f955b9f44
Vision <visiondaniels32@gmail.com>
2023-03-06 11:48:58 +0100

    Feat: Initialized Reviews Page Providers

--- lib/core/locator.dart
+++ lib/core/locator.dart
@@ -1,7 +1,10 @@
+import 'package:hightable_mobile_v2/modules/home/home_dependencies.dart';
+
 import '../modules/authentication/auth_dependencies.dart';
 import 'al_flutter_core.dart';
 
 void setUpLocator(GQLConfig gqlConfig) {
   setupDI(gqlConfig);
   registerAutDependencies();
+  registerHomeDependencies();
 }
